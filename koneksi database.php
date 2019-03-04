0a6c1fbacdf52716d3baa721bcf8a3fec60a4222
nabilajr <46430680+nabilajr@users.noreply.github.com>
2019-03-04 19:11:19 +0700

    Add files via upload

---
+++ koneksi database.php
@@ -0,0 +1,13 @@
+<?php
+    $host = "localhost"; //host database
+    $username = "root"; //username database
+    $password = ""; //password databse
+
+    $koneksi = new mysqli($host, $username, $password); //membuat koneksi ke server mysql
+
+    if ($koneksi->connect_error) {
+        die("koneksi ke database gagal");
+    } else {
+        echo "koneksi ke database berhasil";
+    }
+?>
---
+++ koneksi database3.php
@@ -0,0 +1,11 @@
+<?php
+    $host = "localhost"; //host database
+    $username = "root"; //username database
+    $password = ""; //password databse
+    $db = "profile";
+    $koneksi = new mysqli($host, $username, $password, $db); //membuat koneksi ke server mysql
+
+    if ($koneksi->connect_error) {
+        die("koneksi ke database gagal");
+    }
+?>
